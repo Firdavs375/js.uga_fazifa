59e8c7dc591defff612614b8d7760f96672a8888
Firdavs375 <firdavss.3755@gmail.com>
2023-03-24 16:20:55 +0500

    flex

---
+++ index.js
@@ -0,0 +1,12 @@
+let inp = document.querySelector("input")
+let btn = document.querySelector("button")
+
+
+btn.onclick = () => {
+    if (inp.value == "Firdavs") {
+        document.write("togri topdingiz")
+    }
+    else {
+        document.write("hatto")
+    }
+}
