199dbba43ba1debec8d3b0c699cbec744bed6800
Firdavs375 <firdavss.3755@gmail.com>
2023-03-28 19:44:19 +0500

    flex

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let inp = document.querySelector("input")
-let btn = document.querySelector("button")
+const qiymat = +prompt("Sovg'ani narxini kiriting")
 
+alert(a(50, 50, 50, 50, qiymat));
+function a(a, b, c, d, e) {
+    let num = a + b + c + d
+    if (num <= e) {
+        return "Pul yetarli"
+    } else {
+        return "Pul yetarli emas"
+    }
 
-btn.onclick = () => {
-    if (inp.value == "Firdavs") {
-        document.write("togri topdingiz")
-    }
-    else {
-        document.write("hatto")
-    }
 }
